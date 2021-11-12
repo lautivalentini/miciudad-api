8b079c12783a5e8e56d092f18b18758cac61b296
Lautaro Valentini <lautiivalentini@gmail.com>
2021-11-12 15:45:06 -0300

    Revert "feat: update MongoDB connection"
    This reverts commit 42615777de3b5f473f640db7493f2220ed240e80.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const dotenv = require("dotenv");
-
-const env = process.env.NODE_ENV;
-
-const envFile = env ? `.env.${env}` : ".env";
-dotenv.config({ path: envFile });
 
 const port = process.env.PORT || 8080;
 
 app.use(cors());
 
 app.listen(port, () => {
-  console.log(`Runing on port: ${port}`);
-});
+    console.log(`Runing on port: ${port}`);
+})
 
 module.export = app;
